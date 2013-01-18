dc51888950a6f0c8318dc14b685ff16e44e347ef
Thomas Belin <t.belin@doyoubuzz.com>
2013-01-18 17:04:28 +0100

    Add the showcase api wrapper

---
+++ Showcase/ShowcaseApiWrapper.php
@@ -0,0 +1,122 @@
+<?php
+
+/**
+ * ShowcaseApiWrapper
+ *
+ * @author Name t.belin@doyoubuzz.com
+ */
+class ShowcaseApiWrapper {
+
+    const API_URL = "http://showcase.dyb.local/app_dev.php/api";
+
+    const API_VERSION = "v1";
+
+    protected $apikey;
+
+    protected $apiSecret;
+
+    public function __construct($apikey, $apiSecret)
+    {
+        $this->apikey = $apikey;
+        $this->apiSecret = $apiSecret;
+    }
+
+    /**
+     * doRequest make a request against the Showcase API
+     *
+     * @param mixed $action the url to call
+     * @param array $userParams the url parameters
+     * @param mixed $body the body of the request
+     * @param string $method the method of the request (AUTO, GET, POST, PUT, DELETE)
+     * @return void
+     */
+    public function doRequest($action, $userParams = array(), $body = null, $method = 'AUTO')
+    {
+        $params = $this->generateParams($userParams);
+        $params = $this->computeHash($params);
+        $url = $this->generateUrl($action, $params);
+        $computedMethod = $body === null ? 'GET' : 'POST';
+        $curlMethod = $method == 'AUTO' ? $computedMethod : $method;
+
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_TIMEOUT, 30);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $curlMethod);
+
+        if ($body) {
+            if(is_array($body)) {
+                $body = http_build_query($body);
+            }
+            curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Length: ' . strlen($body), 'Content-Type:text/xml'));
+            curl_setopt($ch, CURLOPT_POSTFIELDS, $body);
+        }
+
+        $content    = curl_exec($ch);
+        $result     = array();
+        $info       = curl_getinfo($ch);
+        $code       = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if($code == 200 || $code == 204) {
+            $result = json_decode($content);
+        } else {
+            throw new Exception('Error while getting data from API on URL ' . $url . ' with return code (' . $code . ') : Request: ' . $body . ' - Response: '. $content);
+        }
+
+        curl_close($ch);
+
+        return $result;
+    }
+
+
+    /*******************
+    * INTERNALS
+    *******************/
+
+    /**
+     * generateUrl
+     *
+     * @param mixed $action
+     * @param mixed $parameters
+     * @return void
+     */
+    protected function generateUrl($action, $parameters)
+    {
+        return self::API_URL . '/' . self::API_VERSION . '/' . $action . '?' . http_build_query($parameters);
+    }
+
+    /**
+     * generateParams generate mandatory parameters for an API request
+     * mandatory parameters include timestamp and apikey
+     *
+     * @return void
+     */
+    protected function generateParams($userParams = array())
+    {
+        $timestamp = time();
+        $mandatoryParams = array(
+            'timestamp' => $timestamp,
+            'apikey' => $this->apikey,
+            'format' => 'json'
+        );
+        return array_merge($userParams, $mandatoryParams);
+    }
+
+    /**
+     * computeHash compute the hash of the request depending on all the parameters
+     *
+     * @param mixed $params
+     * @return void
+     */
+    protected function computeHash($params)
+    {
+        $paramsStr = "";
+        ksort($params);
+        foreach ($params as $paramKey => $paramVal) {
+            $paramsStr .= $paramVal;
+        }
+        $hash = md5($paramsStr . $this->apiSecret);
+        return array_merge($params, array('hash' => $hash));
+    }
+}
---
+++ Showcase/index.php
@@ -0,0 +1,155 @@
+<?php
+    require_once('./ShowcaseApiWrapper.php');
+
+    const USERS_REQUEST = 0;
+    const USER_REQUEST = 1;
+    const USER_TOKEN_REQUEST = 2;
+    const USER_TAG_REQUEST = 3;
+    const USER_TAG_ASSOCIATION_REQUEST = 4;
+    const USER_TAG_DISSOCIATION_REQUEST = 5;
+    const TAGS_REQUEST = 6;
+    const TAG_REQUEST = 7;
+    const TAG_USERS_REQUEST = 8;
+    const DYB_USER_REQUEST = 9;
+    const DYB_CV_REQUEST = 10;
+    const DYB_EMPLOYMENT_PREFERENCES_REQUEST = 11;
+    const DYB_SEARCH_REQUEST = 12;
+
+    $requestTranslation = array(
+        USERS_REQUEST => "Users",
+        USER_REQUEST => "One user",
+        USER_TOKEN_REQUEST => "User token",
+        USER_TAG_REQUEST => "User tags",
+        USER_TAG_ASSOCIATION_REQUEST => "Associate tag to a user",
+        USER_TAG_DISSOCIATION_REQUEST => "Dissociate tag to a user",
+        TAGS_REQUEST => "Tags",
+        TAG_REQUEST => "One Tag",
+        TAG_USERS_REQUEST =>  "Users associated to a tag",
+        DYB_USER_REQUEST =>  "User's DoYouBuzz profile",
+        DYB_CV_REQUEST =>  "Users's DoYouBuzz resume",
+        DYB_EMPLOYMENT_PREFERENCES_REQUEST => "User's DoYouBuzz employment preferences",
+        DYB_SEARCH_REQUEST => "Search"
+    );
+
+    // Configuration
+    $apikey = "XXXXXXXXXXXX";
+    $apisecret =  "XXXXXXXXXXXX";
+    $userId = 1664;
+    $cvId = 2643;
+    $tagId = 28;
+
+    $isHome = !isset($_GET['request']);
+
+    // if the requested url has no params, just display the home page
+    if ($isHome) { return displayHomePage($requestTranslation); }
+
+    /* Change this with the constants above to test the possible requests to the Showcase API */
+    $request = $_GET['request'];
+
+    $shwApi = new ShowcaseApiWrapper($apikey, $apisecret);
+    $data = null;
+    switch($request) {
+        case USERS_REQUEST:
+            $data = $shwApi->doRequest("users");
+            break;
+        case USER_REQUEST:
+            $data = $shwApi->doRequest("users/". $userId, array('isIdOrigin' => 1));
+            break;
+        case USER_TOKEN_REQUEST:
+            $data = $shwApi->doRequest("users/". $userId ."/token", array('isIdOrigin' => 1));
+            break;
+        case USER_TAG_REQUEST:
+            $data = $shwApi->doRequest("users/" . $userId . "/tags", array('isIdOrigin' => 1));
+            break;
+        case USER_TAG_ASSOCIATION_REQUEST:
+            echo '
+                <pre>
+                $data = $shwApi->doRequest("users/" . $userId . "/associateTags",
+                    array("isIdOrigin" => 1),
+                    array("tags" => array($tagId)),
+                    "PUT"
+                );
+                </pre>
+            ';
+            break;
+        case USER_TAG_DISSOCIATION_REQUEST:
+            echo '
+                <pre>
+                $data = $shwApi->doRequest("users/" . $userId . "/dissociateTags",
+                    array("isIdOrigin" => 1),
+                    array("tags" => array($tagId)),
+                    "PUT"
+                );
+                </pre>
+            ';
+                break;
+        case TAGS_REQUEST:
+            $data = $shwApi->doRequest("tags");
+            break;
+        case TAG_REQUEST:
+            $data = $shwApi->doRequest("tags/". $tagId);
+            break;
+        case TAG_USERS_REQUEST:
+            $data = $shwApi->doRequest("tags/". $tagId ."/users");
+            break;
+        case DYB_USER_REQUEST:
+            $data = $shwApi->doRequest("dyb/user/".$userId, array('isIdOrigin' => 1));
+            break;
+        case DYB_CV_REQUEST:
+            $data = $shwApi->doRequest("dyb/cv/".$cvId, array('isIdOrigin' => 1));
+            break;
+        case DYB_EMPLOYMENT_PREFERENCES_REQUEST:
+            $data = $shwApi->doRequest("dyb/employmentpreferences/". $userId, array('isIdOrigin' => 1));
+            break;
+        case DYB_SEARCH_REQUEST:
+            $searchRequest = '
+                <search>
+                    <queries>
+                        <query>
+                            <term>PHP</term>
+                            <fields>
+                                <in>cv</in>
+                                <in>jobs</in>
+                                <in>educations</in>
+                            </fields>
+                        </query>
+                    </queries>
+                    <filters>
+                        <filter>
+                        </filter>
+                    </filters>
+                </search>
+            ';
+            $data = $shwApi->doRequest("dyb/search", array(), $searchRequest);
+            break;
+    }
+    if ($data) { echo json_encode($data); }
+
+
+
+
+    /**
+     * displayHomePage
+     * @access public
+     * @return void
+     */
+    function displayHomePage($requestTranslation)
+    {
+        ?>
+        <html>
+            <head>
+                <style>
+                    a { display:block; }
+                </style>
+            </head>
+            <body>
+            <?php
+                foreach( $requestTranslation as $requestKey => $requestName) {
+                    echo '<a href="/?request=' . $requestKey . '">' . $requestName . '</a>
+                    ';
+                }
+            ?>
+            </body>
+        </html>
+        <?php
+    }
