a32c818617c69389c3a270e5001338057b077a46
Floriane <floriane@doyoubuzz.com>
2017-05-29 17:33:12 +0200

    update of documentation for setting user's manager

--- Showcase/index.php
+++ Showcase/index.php
@@ -42,7 +42,8 @@
         DYB_SEARCH_REQUEST => "Search",
         DYB_METADATA_REQUEST => "Update metadata for user",
         DYB_CV_DISPLAY_CONFIG => "Get the user's display configuration for his resume (color, design id ...)",
-        DYB_CV_LIST => "Get Resumes list (filtered or not)"
+        DYB_CV_LIST => "Get Resumes list (filtered or not)",
+        DYB_SET_MANAGER => "Set user's manager"
     );
 
     // Configuration
@@ -52,6 +53,7 @@
     $cvId = $config['cvId'];
     $tagId = $config['tagId'];
     $searchTerm = $config['searchTerm'];
+    $managerId = $config['managerId'];
 
     $isHome = !isset($_GET['request']);
 
@@ -78,6 +80,9 @@
             break;
         case USER_TAG_REQUEST:
             $data = $shwApi->doRequest("users/" . $userId . "/tags", array('isIdOrigin' => 1));
+            break;
+        case "DYB_SET_MANAGER":
+            $data = $shwApi->doRequest("users/". $userId ."/manager", [ 'managerId' => $managerId ], null, 'PUT');
             break;
         case USER_TAG_ASSOCIATION_REQUEST:
             echo '
